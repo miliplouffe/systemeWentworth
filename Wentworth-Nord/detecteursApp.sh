64fb20987cc2546a13d799cd18ce54b195943149
miliplouffe@gmail.com <miliplouffe@gmail.com>
2024-11-24 05:16:10 -0500

    systemeArrosage

--- Wentworth-Nord/detecteursApp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This is my first script! This is a comment.
-
-cd /home/pi/python
-sudo python detecteursApp.py
--- Wentworth-Nord/systemeAlarme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This is my first script! This is a comment.
-
-cd /home/miliplouffe/python
-sudo python systemeAlarme.py
